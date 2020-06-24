b313b5beccef2eb4e38ac02fc796f802c68c4f0e
Aleksander <alexweb1979@gmail.com>
2020-06-24 22:59:23 +0300

    Finish resize table

---
+++ src/components/table/table.resize.js
@@ -0,0 +1,50 @@
+import {$} from '@core/Dom';
+
+export function resizeHendler($root, event) {
+  const $resizer = $(event.target)
+  const $parent = $resizer.closest('[data-type="resizable"]')
+  const coords = $parent.getCoords()
+  const type = $resizer.data.resize
+  let value
+
+  $resizer.css({opacity: 1})
+
+  if (type === 'col') {
+    $resizer.css({height: 100 + 'vh'})
+
+    document.onmousemove = (e) => {
+      const delta = e.pageX - coords.right
+      value = coords.width + delta
+      $resizer.css({right: -delta + 'px'})
+    }
+  } else if (type === 'row') {
+    $resizer.css({width: 100 + 'vw'})
+
+    document.onmousemove = (e) => {
+      const delta = e.pageY - coords.bottom
+      value = coords.height + delta
+      $resizer.css({bottom: -delta + 'px'})
+    }
+  }
+
+  document.onmouseup = () => {
+    document.onmousemove = null
+    document.onmouseup = null
+
+    $resizer.css({
+      opacity: 0,
+      right: 0,
+      bottom: 0
+    })
+
+    if (type === 'col') {
+      $resizer.css({height: 'auto'})
+      $parent.css({width: value + 'px'})
+      $root.findAll(`[data-col="${$parent.data.col}"]`)
+          .forEach(el => el.style.width = value + 'px')
+    } else if (type === 'row') {
+      $resizer.css({width: 'auto'})
+      $parent.css({height: value + 'px'})
+    }
+  }
+}
