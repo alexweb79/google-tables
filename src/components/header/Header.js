81cdefa7b56cef8a5282be51da94053f9332f4da
Aleksander <alexweb1979@gmail.com>
2020-07-26 21:05:19 +0300

    Finish redux and table logic

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,4 +1,7 @@
 import {ExcelComponent} from '@core/ExcelComponent';
+import {$} from '@core/Dom';
+import {changeTitle} from '@/redux/actions';
+import {debounce} from '@core/utils';
 
 export class Header extends ExcelComponent {
   static className = 'excel__header'
@@ -6,17 +9,29 @@ export class Header extends ExcelComponent {
   constructor($root, options) {
     super($root, {
       name: 'Header',
+      listeners: ['input'],
       ...options
     });
   }
 
+  prepare() {
+    this.onInput = debounce(this.onInput, 300)
+  }
+
   toHTML() {
+    const title = this.store.getState().title
     return `
-      <input class="input" type="text" value="Новая таблица" />
+      <input class="input" type="text" value="${title}" />
       <div>
         <div class="button"><i class="material-icons">delete</i></div>
         <div class="button"><i class="material-icons">exit_to_app</i></div>
       </div>
     `
+  }
+
+  onInput(event) {
+    console.log('onInput')
+    const $target = $(event.target)
+    this.$dispatch(changeTitle($target.text()))
   }
 }
--- src/core/ExcelComponent.js
+++ src/core/ExcelComponent.js
@@ -5,6 +5,8 @@ export class ExcelComponent extends DomListener {
     super($root, options.listeners)
     this.name = options.name || ''
     this.emitter = options.emitter
+    this.subscribe = options.subscribe || []
+    this.store = options.store
     this.unsubscrubers = []
 
     this.prepare()
@@ -27,6 +29,16 @@ export class ExcelComponent extends DomListener {
   $on(event, fn) {
     const unsub = this.emitter.subscribe(event, fn)
     this.unsubscrubers.push(unsub)
+  }
+
+  $dispatch(action) {
+    this.store.dispatch(action)
+  }
+
+  storeChanged() {}
+
+  isWatching(key) {
+    return this.subscribe.includes(key)
   }
 
   // Инициализируем компоненты
